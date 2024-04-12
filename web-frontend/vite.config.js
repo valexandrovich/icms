259c139570cc6970d4819a04020de9bfa82438db
V.Alexandrovich <beloenko23@gmail.com>
2024-04-12 10:54:13 +0200

    Server poling in web

--- web-frontend/vite.config.js
+++ web-frontend/vite.config.js
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         // target: 'http://23.88.42.241:9000',
@@ -15,6 +16,9 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
+    },
+    watch: {
+      usePolling: true
     }
   },
   plugins: [
